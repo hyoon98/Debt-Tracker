7653944c30b893fd6d65c0c8ae5337a1264a0436
hya62 <hansoo_yoon@sfu.ca>
2021-08-21 21:04:15 -0700

    setup different pages

--- client/debt-tracker/src/index.js
+++ client/debt-tracker/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import reportWebVitals from './reportWebVitals'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+//Pages
+import App from './pages/App';
+import Owed from './pages/Owed';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route exact path='/'>
+        <App />
+      </Route>
+      <Route exact path='/owed'>
+        <Owed />
+      </Route>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
